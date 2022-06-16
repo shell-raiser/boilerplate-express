09f032a6fdcf3c763cc67483b25d96c495b78fb4
Shell Raiser <78999739+shell-raiser@users.noreply.github.com>
2022-06-16 02:59:16 +0000

    implemented root level request logger

--- myApp.js
+++ myApp.js
@@ -6,13 +6,17 @@ app.get('/',myFunction);
 function myFunction(req, res) {
   res.sendFile(__dirname +'/views/index.html') ;
 }
-app.get('/json',myFunction2);
-function myFunction2(req, res) {
-    if (process.env.MESSAGE_STYLE=='uppercase'){
-      res.json({"message": "HELLO JSON"}) ;  
-    } else {
-        res.json({"message": "Hello json"}); 
-    }
+//app.get('/json',myFunction2);
+app.use(myFunction2)
+function myFunction2(req, res, next) {
+    //if (process.env.MESSAGE_STYLE=='uppercase'){
+      //res.json({"message": "HELLO JSON"}) ;  
+    //} else {
+        //res.json({"message": "Hello json"}); 
+    //}
+    console.log(req.method,req.path,"-",req.ip);
+      next();
+
 }
 
 
